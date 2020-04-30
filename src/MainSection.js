7313605ecbbac643aa1333511545951ddb409a03
Adam Osburn <wizardofosburn@gmail.com>
2020-04-30 19:23:54 -0400

    created export function (default) set state for title, I think.

--- src/MainSection.js
+++ src/MainSection.js
@@ -1,4 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
- const [title, setTitle] = useState(props.title);
- console.log('This is Header/title: ', title)
+export default function MainSection(props) {
+console.log('mainSection props = ', props)
+  const [title] = useState(props.data.title);
+  console.log("This is Title: ", title);
+
+  return (
+    <div className = 'main-section'>
+      <h1 className="title" >{title}</h1>
+      <p className='description' >{props.explanation}</p>
+      <img src={props.url} alt={props.explanation}/>
+      {console.log('This is MainSection props: ', props)}
+      <p className='date'>{props.date}</p>
+    </div>
+  )
+}
