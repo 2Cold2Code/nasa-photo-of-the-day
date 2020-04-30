ca07ce742c7ca26e425506dfd2acbe5e927a7f98
Adam Osburn <wizardofosburn@gmail.com>
2020-04-30 19:21:34 -0400

    imported MainSection, placed axios call within function call, changed prop name from 'props' to 'data'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import scriptInsertAxios from './insertAxios'
 import Header from './Header'
+import MainSection from './MainSection'
+
 scriptInsertAxios();
 
-let holdResponse = [];
-const axCall = axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
-  .then((response) => {
-    console.log(response);
-    holdResponse.push(response.data);
-  })
-  .catch((e) => console.log(e));
+
 
 
 
 function App() {
-  if (!holdResponse.photoOfTheDay) return <h3>Loading...</h3>
+useEffect( () => {  
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
+  .then((response) => {
+    console.log(response);
+    setHoldResponse(response.data);
+  })
+  .catch((e) => console.log(e));
+  }, [])
+
+  const [holdResponse, setHoldResponse] = useState('')
+  
   return (
     <div className="App">
      <Header className='header' />
-     <MainSection className='main-body' props={holdResponse} />
+     <MainSection className='main-body' data={holdResponse} />
     </div>
   );
 }
 
-export default App;
+export default App
