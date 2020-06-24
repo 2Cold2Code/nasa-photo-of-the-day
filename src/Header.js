b5a0350f46b15b4917aec81bc6dd9fe1378c2187
Adam Osburn <wizardofosburn@gmail.com>
2020-06-23 21:03:03 -0400

    formatted file

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,16 @@
-import React, {useState} from "react";
-import Nav from './Nav';
+import React, { useState } from "react";
+import Nav from "./Nav";
 
 export default function Header() {
-const textArr = ['Home', 'About', `Tell us about${'\n'}how you feel${'\n'}about our nav${'\n'} not working!`]
-const [text, setText] = useState(textArr);
+  const textArr = [
+    "Home",
+    "About",
+    `Tell us about${"\n"}how you feel${"\n"}about our nav${"\n"} not working!`,
+  ];
+  const [text, setText] = useState(textArr);
   return (
     <div>
-      <Nav className='navigation' text={text}/>
+      <Nav className="navigation" text={text} />
     </div>
   );
 }
