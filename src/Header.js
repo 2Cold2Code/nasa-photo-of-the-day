7a2cd155ca64ac577b95187b308c23466ee43cb9
Adam Osburn <wizardofosburn@gmail.com>
2020-06-23 20:31:13 -0400

    added useState statement

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from './Nav';
 
 export default function Header() {
 const textArr = ['Home', 'About', `Tell us about${'\n'}how you feel${'\n'}about our nav${'\n'} not working!`]
-const [text, setText] = useState(...textArr);
+const [text, setText] = useState(textArr);
   return (
     <div>
       <Nav className='navigation' text={text}/>
