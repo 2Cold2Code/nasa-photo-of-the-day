71602303a4d96daa06b8622619424b2a9212a55e
Adam Osburn <wizardofosburn@gmail.com>
2020-06-23 21:02:36 -0400

    removed excess imports, played with the desired date for photo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import scriptInsertAxios from './insertAxios'
-import Header from './Header'
-import MainSection from './MainSection'
 
-scriptInsertAxios();
-
-
-
-
+import Header from "./Header";
+import MainSection from "./MainSection";
 
 function App() {
-useEffect( () => {  
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
-  .then((response) => {
-    console.log(response);
-    setHoldResponse(response.data);
-  })
-  .catch((e) => console.log(e));
-  }, [])
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-01-03"
+      )
+      .then((response) => {
+        console.log(response);
+        setHoldResponse(response.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
-  const [holdResponse, setHoldResponse] = useState('')
-  
+  const [holdResponse, setHoldResponse] = useState("");
+
   return (
     <div className="App">
-     <Header className='header' />
-     <MainSection className='main-body' data={holdResponse} />
+      <Header className="header" />
+      <MainSection className="main-body" data={holdResponse} />
     </div>
   );
 }
 
-export default App
+export default App;
